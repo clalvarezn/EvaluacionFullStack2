3d06596ed4259d0effe43040f8c794fafaf9329a
clalvarezn <cl.alvarezn@duocuc.cl>
2026-06-11 00:37:58 -0400

    Enhance Producto model with validation constraints
    Added validation annotations to Producto fields for better data integrity.

--- producto-service/producto-service/src/main/java/com/lovable/producto_service/model/Producto.java
+++ producto-service/producto-service/src/main/java/com/lovable/producto_service/model/Producto.java
@@ -20,27 +20,36 @@ public class Producto {
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer idProducto;
 
+    @NotBlank(message = "Debe ingresar el nombre del producto")
     @Column(nullable = false)
     private String nombreProducto;
 
+    @NotBlank(message = "Debe ingresar la descripción")
     @Column(nullable = false)
     private String descripcion;
 
+    @NotBlank(message = "Debe ingresar la talla")
     @Column(nullable = false)
     private String talla;
 
+    @NotBlank(message = "Debe ingresar el color")
     @Column(nullable = false)
     private String color;
 
+    @NotNull(message = "Debe ingresar el precio del producto")
+    @Min(value = 1, message = "El precio debe ser mayor a 0")
     @Column(nullable = false)
     private Integer precio;
 
+    @NotNull(message = "Debe ingresar la dirección de la imagen")
     @Column(nullable = false)
     private String imagen;
 
     @Column(nullable = false)
     private Boolean estadoProducto;
 
+    @NotNull(message = "Debe ingresar el stock actual")
+    @PositiveOrZero(message = "El stock no puede ser un número negativo")
     @Column(nullable = false)
     private Integer stockActual;
 
